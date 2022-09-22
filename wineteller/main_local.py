d1cb9d938daa29b8e5a1ab43d32a4472dcb033dc
Hyunoo Chang chyunoo3@gmail.com <chyunoo3@gmail.com>
2022-09-22 21:24:02 +0200

    création d'interface centrale

---
+++ wineteller/main_local.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import os
+
+from wineteller.import_data import get_data
+
+def preprocess_and_train() :
+
+    print("\n⭐️ use case: preprocess and train basic")
+
+    get_data()
+
+
+
+if __name__ == '__main__':
+    preprocess_and_train()
