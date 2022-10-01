584564dd346352182a67e70fc69ebce96940c98b
Hyunoo Chang chyunoo3@gmail.com <chyunoo3@gmail.com>
2022-10-01 03:19:02 +0200

    added training module and updated main local accordingly

--- wineteller/interface/main_local.py
+++ wineteller/interface/main_local.py
@@ -3,7 +3,9 @@ import pandas as pd
 import os
 
 from wineteller.import_data import get_data, clean_wine_data, get_test_data
+from wineteller.model import merge_review_vectors, vectorize_reviews, word_embeddings
 from wineteller.preprocessor import preprocess_text
+from wineteller.registry import save_model, load_model
 
 def preprocess_and_train() :
     """
@@ -21,7 +23,21 @@ def preprocess_and_train() :
     # preprocess data
     df_pp = preprocess_text(cleaned)
 
-    return df_pp
+    # train model
+    trained_w2v = word_embeddings(df_pp)
+
+    # save model
+    save_model(trained_w2v)
+
+    # retrieve wine review vectors
+    wine_review_vectors = vectorize_reviews(cleaned, trained_w2v)
+
+    # merge in wine dataset
+    vectorized_df = merge_review_vectors(wine_review_vectors, cleaned)
+
+    print(vectorized_df.shape)
+    print(vectorized_df.head())
+
 
 if __name__ == '__main__':
     preprocess_and_train()
